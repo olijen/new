1135f452fff26b48dcc5992a93d041bb51e8a93b
Oleg <olijenius@gmail.com>
2017-02-11 11:10:27 +0200

    Update main.js

--- static/js/main.js
+++ static/js/main.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
 	console.log("Hello!");
+	event.originalEvent.defaultPrevented;
 	navigator.geolocation.getCurrentPosition(function(position) {
 
 	// Текущие координаты.
