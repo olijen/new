075055d4362217ba411ddd613e6aeca9f3d43bfb
Oleg <olijenius@gmail.com>
2017-02-11 11:05:57 +0200

    Update main.js

--- static/js/main.js
+++ static/js/main.js
@@ -1,3 +1,24 @@
 $(document).ready(function() {
 	console.log("Hello!");
+	navigator.geolocation.getCurrentPosition(function(position) {
+
+	// Текущие координаты.
+	var lat = position.coords.latitude;
+	var lng = position.coords.longitude;
+	alert(lat + "" + lng);
+	// Отрисовка карты.
+	/*var map = new GMaps({
+	    el: '#map',
+	    lat: lat,
+	    lng: lng
+	});
+
+	map.addMarker({
+	    lat: lat,
+	    lng: lng
+	});
+
+	});*/
+
+	});
 });
